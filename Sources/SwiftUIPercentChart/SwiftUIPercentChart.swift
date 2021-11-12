abf65f00349872045dcbf31738859b0a321a8737
Mehmet ateş <Mehmet1095@icloud.com>
2021-11-12 23:36:20 +0300

    Comment lines and debug view add

--- Sources/SwiftUIPercentChart/SwiftUIPercentChart.swift
+++ Sources/SwiftUIPercentChart/SwiftUIPercentChart.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 
 @available(iOS 13.0, *)
+/// Creates a horizontal chart that calculates percentile slices
 public struct SwiftUIPercentChart : View {
     private var data: Array<Double>
     private var percentValue : Double
@@ -24,11 +25,17 @@ public struct SwiftUIPercentChart : View {
     private var screenSize = UIScreen.main.bounds
     private var screenRatio : Double
     private var backgroundColor : Color
+    // Required variables
     public init(data:Array<Double> , percentValue : Double , colorData : Array<LinearGradient> , screenRatio : Double, backgroundColor : Color){
+        // Data of the graphics
         self.data = data
+        // The value that determines the percentage of the bar
         self.percentValue = percentValue
+        // The value set the chart colors
         self.colorData = colorData
+        // The value for setting the width of the chart according to the device screen (among to 0 - 1)
         self.screenRatio = screenRatio
+        // The value set the backgorund bar color
         self.backgroundColor = backgroundColor
     }
     public var body: some View{
@@ -73,9 +80,8 @@ public struct SwiftUIPercentChart : View {
                     }
                 }else
                 {
-                    
+                    Text("Make the app version ios 15")
                 }
-                
             }.scaleEffect(self.valueScale)
                 .onTouchGesture(
                     touchBegan: { withAnimation { self.valueScale = 1.2 } },
@@ -88,11 +94,11 @@ public struct SwiftUIPercentChart : View {
     }
 }
 
-////#if DEBUG
-//struct SwiftUIPercentChart_Previews : PreviewProvider {
-//    @available(iOS 13.0, *)
-//    static var previews: some View {
-//        SwiftUIPercentChart()
-//    }
-//}
-////#endif
+#if DEBUG
+struct SwiftUIPercentChart_Previews : PreviewProvider {
+    @available(iOS 13.0, *)
+    static var previews: some View {
+        SwiftUIPercentChart(data: [10,20,30], percentValue: 100 , colorData: [] ,screenRatio: 0.8,backgroundColor: .primary)
+    }
+}
+#endif
