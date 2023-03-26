fefeeead7b7e25ae87ff3847774363eb59100db1
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-03-26 18:52:53 +0300

    View create methods updated & themes and flexibility added

--- Sources/SwiftUIPercentChart/Helpers.swift
+++ Sources/SwiftUIPercentChart/Helpers.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @available(iOS 13.0, *)
 extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape( RoundedCorner(radius: radius, corners: corners) )
+        clipShape(RoundedCorner(radius: radius, corners: corners))
     }
 }
 
@@ -24,51 +24,168 @@ struct RoundedCorner: Shape {
     }
 }
 
-@available(iOS 13.0, *)
-class ColorHelper{
-    let defaultColors = [LinearGradient(colors: [.red,.orange], startPoint: .top, endPoint: .bottom),LinearGradient(colors: [.blue.opacity(0.8),.blue], startPoint: .top, endPoint: .bottom),LinearGradient(colors: [.purple.opacity(0.8),.purple], startPoint: .top, endPoint: .bottom),LinearGradient(colors: [.green.opacity(0.8),.green], startPoint: .top, endPoint: .bottom)]
+public enum Themes: String, CaseIterable {
+    case light = "Light"
+    case dark = "Dark"
+    case love = "Love"
+    case ocean = "Ocean"
+    case natural = "Natural"
+    case colorful = "Colorful"
+    case sunset = "Sunset"
+    case neon = "Neon"
 }
 
 @available(iOS 13.0, *)
-struct TouchGestureViewModifier: ViewModifier {
-    let touchBegan: () -> Void
-    let touchEnd: (Bool) -> Void
+public struct ColorThemes {
+    static let light: [Color] = [
+        .init(hex: "fec5bb"),
+        .init(hex: "fcd5ce"),
+        .init(hex: "fae1dd"),
+        .init(hex: "f8edeb"),
+        .init(hex: "e8e8e4"),
+        .init(hex: "d8e2dc"),
+        .init(hex: "ece4db"),
+        .init(hex: "ffd7ba"),
+        .init(hex: "fec89a")
+    ]
     
-    @State private var hasBegun = false
-    @State private var hasEnded = false
+    static let dark: [Color] = [
+        .init(hex: "181818"),
+        .init(hex: "282828"),
+        .init(hex: "404048"),
+        .init(hex: "505860"),
+        .init(hex: "66707a"),
+        .init(hex: "381820"),
+        .init(hex: "501820"),
+        .init(hex: "502028")
+    ]
     
-    private func isTooFar(_ translation: CGSize) -> Bool {
-        let distance = sqrt(pow(translation.width, 2) + pow(translation.height, 2))
-        return distance >= 20.0
-    }
+    static let love: [Color] = [
+        .init(hex: "fff0f3"),
+        .init(hex: "ffccd5"),
+        .init(hex: "ffb3c1"),
+        .init(hex: "ff8fa3"),
+        .init(hex: "ff758f"),
+        .init(hex: "ff4d6d"),
+        .init(hex: "a4133c"),
+        .init(hex: "800f2f"),
+        .init(hex: "590d22")
+    ]
     
-    func body(content: Content) -> some View {
-        content.gesture(DragGesture(minimumDistance: 0)
-                            .onChanged { event in
-            guard !self.hasEnded else { return }
-            
-            if self.hasBegun == false {
-                self.hasBegun = true
-                self.touchBegan()
-            } else if self.isTooFar(event.translation) {
-                self.hasEnded = true
-                self.touchEnd(false)
-            }
-        }
-                            .onEnded { event in
-            if !self.hasEnded {
-                let success = !self.isTooFar(event.translation)
-                self.touchEnd(success)
-            }
-            self.hasBegun = false
-            self.hasEnded = false
-        })
-    }
+    static let ocean: [Color] = [
+        .init(hex: "a9d6e5"),
+        .init(hex: "89c2d9"),
+        .init(hex: "61a5c2"),
+        .init(hex: "468faf"),
+        .init(hex: "2c7da0"),
+        .init(hex: "2a6f97"),
+        .init(hex: "014f86"),
+        .init(hex: "01497c"),
+        .init(hex: "013a63"),
+        .init(hex: "012a4a")
+    ]
+    
+    static let natural: [Color] = [
+        .init(hex: "d8f3dc"),
+        .init(hex: "b7e4c7"),
+        .init(hex: "95d5b2"),
+        .init(hex: "74c69d"),
+        .init(hex: "52b788"),
+        .init(hex: "40916c"),
+        .init(hex: "2d6a4f"),
+        .init(hex: "1b4332"),
+        .init(hex: "081c15")
+    ]
+    
+    static let colorful: [Color] = [
+        .init(hex: "ffadad"),
+        .init(hex: "ffd6a5"),
+        .init(hex: "fdffb6"),
+        .init(hex: "caffbf"),
+        .init(hex: "9bf6ff"),
+        .init(hex: "a0c4ff"),
+        .init(hex: "bdb2ff"),
+        .init(hex: "ffc6ff"),
+        .init(hex: "fffffc")
+    ]
+    
+    static let sunset: [Color] = [
+        .init(hex: "ff7b00"),
+        .init(hex: "ff8800"),
+        .init(hex: "ff9500"),
+        .init(hex: "ffa200"),
+        .init(hex: "ffaa00"),
+        .init(hex: "ffb700"),
+        .init(hex: "ffd000"),
+        .init(hex: "ffea00"),
+    ]
+    
+    static let neon: [Color] = [
+        .init(hex: "f72585"),
+        .init(hex: "b5179e"),
+        .init(hex: "7209b7"),
+        .init(hex: "560bad"),
+        .init(hex: "3a0ca3"),
+        .init(hex: "3f37c9"),
+        .init(hex: "4361ee"),
+        .init(hex: "4895ef"),
+        .init(hex: "4cc9f0")
+    ]
 }
+
 @available(iOS 13.0, *)
-extension View {
-    func onTouchGesture(touchBegan: @escaping () -> Void,
-                        touchEnd: @escaping (Bool) -> Void) -> some View {
-        modifier(TouchGestureViewModifier(touchBegan: touchBegan, touchEnd: touchEnd))
+public extension Color {
+    static func getColor(_ colorTheme: [Color], _ index: Int) -> Color {
+        if colorTheme.indices.contains(index) {
+            return colorTheme[index]
+        } else {
+            return colorTheme[index % colorTheme.count]
+        }
+    }
+    
+    static func themeColor(by index: Int, with theme: Themes) -> Color {
+        switch theme {
+        case .light:
+            return getColor(ColorThemes.light, index)
+        case .dark:
+            return getColor(ColorThemes.dark, index)
+        case .love:
+            return getColor(ColorThemes.love, index)
+        case .ocean:
+            return getColor(ColorThemes.ocean, index)
+        case .natural:
+            return getColor(ColorThemes.natural, index)
+        case .colorful:
+            return getColor(ColorThemes.colorful, index)
+        case .sunset:
+            return getColor(ColorThemes.sunset, index)
+        case .neon:
+            return getColor(ColorThemes.neon, index)
+        }
+    }
+    
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
--- Sources/SwiftUIPercentChart/SwiftUIPercentChart.swift
+++ Sources/SwiftUIPercentChart/SwiftUIPercentChart.swift
@@ -1,96 +1,60 @@
 import SwiftUI
 
-
 @available(iOS 13.0, *)
 /// Creates a horizontal chart that calculates percentile slices
-public struct SwiftUIPercentChart : View {
-    private var data: Array<Double>
-    private var percentValue : Double
-    private var applyPercent : Double{
-        if self.data.reduce(0, +) > percentValue{
-            return self.data.reduce(0, +)
-        }else{
-            return self.percentValue
-        }
-    }
-    private var colorData : Array<LinearGradient>
-    private var applyColor : Array<LinearGradient>{
-        if self.colorData.isEmpty{
-            return ColorHelper().defaultColors
-        }else{
-            return colorData
-        }
-    }
-    @State var valueScale = CGFloat(1)
-    private var screenSize = UIScreen.main.bounds
-    private var screenRatio : Double
-    private var backgroundColor : Color
-    // Required variables
-    public init(data:Array<Double> , percentValue : Double , colorData : Array<LinearGradient> , screenRatio : Double, backgroundColor : Color){
-        // Data of the graphics
+public struct SwiftUIPercentChart: View {
+    private var data: [Double]
+    private var percentValue: Double
+    private var theme: Themes
+    
+    init(data: [Double] = [], percentValue: Double? = nil, theme: Themes = .dark) {
         self.data = data
-        // The value that determines the percentage of the bar
-        self.percentValue = percentValue
-        // The value set the chart colors
-        self.colorData = colorData
-        // The value for setting the width of the chart according to the device screen (among to 0 - 1)
-        self.screenRatio = screenRatio
-        // The value set the backgorund bar color
-        self.backgroundColor = backgroundColor
+        self.percentValue = percentValue ?? 0 < Double(data.reduce(0, +)) ? Double(data.reduce(0, +)) : percentValue ?? Double(data.reduce(0, +))
+        self.theme = theme
     }
-    public var body: some View{
-        ZStack(alignment:.leading){
-            Rectangle()
-                .foregroundColor(self.backgroundColor.opacity(0.3))
-                .frame(width: screenSize.width * self.screenRatio, height: 20)
-                .cornerRadius(16)
-            HStack(spacing:0){
-                if #available(iOS 15.0, *){
-                    if self.data.count > 0 {
-                        if self.data.count == 1{
+    
+    public var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(.gray.opacity(0.2))
+                HStack(spacing: 0) {
+                    ForEach(Array(zip(data.indices, data)), id: \.0) { index, value in
+                        if index == 0 {
                             Rectangle()
-                                .frame(width: calWidth(value: self.data[0]) , height: 20)
-                                .cornerRadius(16)
-                                .foregroundStyle(self.applyColor[0])
-                            
-                        }else{
+                                .cornerRadius(cellRadius(by: index), corners: [.topLeft, .bottomLeft])
+                                .foregroundColor(.themeColor(by: index, with: theme))
+                                .frame(width: cellWidth(by: index, proxy.size.width))
+                        } else if index == data.count - 1 {
                             Rectangle()
-                                .frame(width: calWidth(value: self.data[0]), height: 20)
-                                .cornerRadius(16,corners:[.topLeft,.bottomLeft])
-                                .foregroundStyle(self.applyColor[0])
-                            ForEach(0..<self.data.count) {
-                                a in
-                                if a == 0{
-                                    
-                                }
-                                else if a == self.data.count - 1 {
-                                    Rectangle()
-                                        .frame(width: calWidth(value: self.data[a]), height: 20)
-                                        .cornerRadius(16,corners:[.topRight,.bottomRight])
-                                        .foregroundStyle(self.applyColor[a % self.applyColor.count])
-                                }
-                                else{
-                                    Rectangle()
-                                        .frame(width: calWidth(value: self.data[a]), height: 20)
-                                        .foregroundStyle(self.applyColor[a % self.applyColor.count])
-                                    
-                                }
-                            }
+                                .cornerRadius(cellRadius(by: index), corners: [.topRight, .bottomRight])
+                                .foregroundColor(.themeColor(by: index, with: theme))
+                                .frame(width: cellWidth(by: index, proxy.size.width))
+                        } else {
+                            Rectangle()
+                                .foregroundColor(.themeColor(by: index, with: theme))
+                                .frame(width: cellWidth(by: index, proxy.size.width))
                         }
                     }
-                }else
-                {
-                    Text("Make the app version ios 15")
+                    
+                    if percentValue > data.reduce(0, +) {
+                        Spacer()
+                    }
                 }
-            }.scaleEffect(self.valueScale)
-                .onTouchGesture(
-                    touchBegan: { withAnimation { self.valueScale = 1.2 } },
-                    touchEnd: { _ in withAnimation { self.valueScale = 1.0 } }
-                )
+            }
         }
     }
-    func calWidth(value : Double) -> Double{
-        return (value * (screenSize.width * self.screenRatio))/applyPercent
+    
+    private func cellCapacity(by index: Int) -> Double {
+        return (data[index] * 100) / percentValue
+    }
+    
+    private func cellWidth(by index: Int, _ width: CGFloat) -> Double {
+        return  (cellCapacity(by: index) * width) / 100
+    }
+    
+    private func cellRadius(by index: Int) -> Double {
+        return 500 / cellCapacity(by: index)
     }
 }
 
@@ -98,7 +62,28 @@ public struct SwiftUIPercentChart : View {
 struct SwiftUIPercentChart_Previews : PreviewProvider {
     @available(iOS 13.0, *)
     static var previews: some View {
-        SwiftUIPercentChart(data: [10,20,30], percentValue: 100 , colorData: [] ,screenRatio: 0.8,backgroundColor: .primary)
+        let screenSize = UIScreen.main.bounds
+        
+        VStack {
+            ForEach(Themes.allCases, id: \.self) { theme in
+                if #available(iOS 15.0, *) {
+                    VStack(alignment: .leading) {
+                        SwiftUIPercentChart(data: [50, 40, 30, 20, 30, 50, 30, 10, 20, 50], percentValue: 350, theme: theme)
+                            .frame(width: screenSize.width * 0.7, height: 10)
+                        Text(theme.rawValue)
+                            .bold()
+                    }.padding()
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(10)
+                } else {
+                    VStack(alignment: .leading) {
+                        SwiftUIPercentChart(data: [50, 40, 30, 20, 30, 50, 30, 10, 20, 50], percentValue: 350, theme: theme)
+                            .frame(width: screenSize.width * 0.7, height: 10)
+                        Text(theme.rawValue)
+                    }
+                }
+            }
+        }
     }
 }
 #endif
